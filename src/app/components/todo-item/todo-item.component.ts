03399cb1a45a4d2874bd25b1d4328c3406bca235
fouad riad <fouad.riad.955@gmail.comgit config --global user.name fouad>
2024-03-13 13:02:40 +0100

    todo

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import{Todo } from '../../models/todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+export class TodoItemComponent {
+  @Input()
+todo: Todo | undefined
+@Output()
+onRichiestaCompletamento = new EventEmitter<Todo>()
+richiediCompletamento (){
+  this.onRichiestaCompletamento.emit(this.todo)
+}
+}
